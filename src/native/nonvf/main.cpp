f2a90a724b38621bd1c4cf5e6bdd3aa2ff15cc71
DaniGoodwin <danmarkhunter13072006@gmail.com>
2021-10-02 20:26:30 +0300

    Commit before moving to new PC

--- src/native/nonvf/main.cpp
+++ src/native/nonvf/main.cpp
@@ -4,35 +4,61 @@
 #include <symbol.h>
 #include <nativejs.h>
 #include "config.h"
+#include <typeinfo>
 
-class LevelRendererPlayer;
 class MobEffect {
 	public:
 	static const MobEffect NIGHT_VISION;
 };
 class MobEffectInstance {
 	public:
+	bool isNoCounter() const;
 	int getDuration() const;
 };
 class Actor {
 	public:
 	MobEffectInstance* getEffect(MobEffect const&) const;
 };
-class Mob : public Actor {
-	public:
-};
+class Mob : public Actor {public:};
+class Player : public Mob {public:};
+class LocalPlayer : public Player {public:};
+
+namespace GlobalContext {
+	LocalPlayer* getLocalPlayer();
+}
+
+template<typename smth>
+void logType(const char* what, smth something) {
+	std::string typeinfo = typeid(something).name();
+	const char* typeinfo_c_str = typeinfo.c_str();
+	Logger::debug("TYPELOGGING", "%s is %s", what, typeinfo_c_str);
+	Logger::flush();
+}
 
 class MainModule : public Module {
 public:
 	MainModule(const char* id): Module(id) {};
 
 	virtual void initialize() {
+		logType<MobEffect>("MobEffect::NIGHT_VISION", MobEffect::NIGHT_VISION);
 		DLHandleManager::initializeHandle("libminecraftpe.so", "mcpe");
-		HookManager::addCallback(SYMBOL("mcpe", "_ZN19LevelRendererPlayer19getNightVisionScaleERK3Mobf"), LAMBDA((HookManager::CallbackController* controller, LevelRendererPlayer* lrp, Mob const& mob, float partialTicks), {
+		HookManager::addCallback(
+		SYMBOL("mcpe", "_ZN19LevelRendererPlayer19getNightVisionScaleERK3Mobf"), 
+		LAMBDA((HookManager::CallbackController* controller), {
 			controller->replace();
 			if(!NoNVFConfig::fadeOut) return NoNVFConfig::maxBrightness;
-			int i = mob.getEffect(MobEffect::NIGHT_VISION)->getDuration();
-			return i > NoNVFConfig::fadeTicks ? NoNVFConfig::maxBrightness : i * NoNVFConfig::fadeRate;
+			MobEffectInstance* effect = GlobalContext::getLocalPlayer()->getEffect(MobEffect::NIGHT_VISION);
+			if(effect == nullptr) {
+				// Logger::debug("NONVF", "Night vision MobEffectInstance is nullptr!");
+				// Logger::flush();
+				return 0.0f;
+			}
+			if(effect->isNoCounter()) {
+				return NoNVFConfig::maxBrightness;
+			} else {
+				int duration = effect->getDuration();
+				return duration > NoNVFConfig::fadeTicks ? NoNVFConfig::maxBrightness : duration * NoNVFConfig::fadeRate;
+			}
 		}, ), HookManager::CALL | HookManager::LISTENER | HookManager::CONTROLLER | HookManager::RESULT);
     }
 };
